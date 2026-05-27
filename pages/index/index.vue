7c579eee9fe59fef738077c985b9aff4f7b136d5
coke <13062567585@163.com>
2026-05-28 07:29:54 +0800

    feat: 车辆列表

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
   <view class="content">
     <text class="title">首页</text>
-    <button @click="testReq">测试接口请求</button>
+    <button @click="testReq">车辆详情</button>
 		<navigator url="/pages/login/login">登录</navigator>
     <button style="margin-top: 20rpx" @click="logout">退出登录</button>
   </view>
@@ -15,9 +15,9 @@ import { getHomeListApi } from '@/axios/index'
 
 // 测试请求
 const testReq = async () => {
-	console.log(123)
+
 	uni.navigateTo({
-		url: '/pages/login/login'
+		url: '/pages/car/details?carid=123'
 	})
   // try {
   //   const res = await getHomeListApi({ page: 1 })
