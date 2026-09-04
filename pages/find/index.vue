673c6ae89016487fb984745325e03ee85ef3cd8b
coke <13062567585@163.com>
2026-09-04 21:25:54 +0800

    feat: 我的

---
+++ pages/find/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="container">
+    <!-- #ifndef MP-WEIXIN -->
+    <!-- <custom-nav-bar title="我的"></custom-nav-bar> -->
+    <!-- #endif -->
+
+   
+  </view>
+
+ 
+</template>
+
+<style lang="scss" scoped>
+.container {
+ 
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #fff;
+  position: relative;
+
+
+}
+
+</style>
